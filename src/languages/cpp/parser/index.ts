8a129b8ca0fb85ab48ba90639bd5fce22318131f
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2025-11-17 03:48:12 -0300

    feat: implement C++ parser and semantic analyzer with strategies for syntactic and semantic analysis

---
+++ src/languages/cpp/parser/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Módulo de análise sintática para C++.
+ */
+
+export { Parser } from './parser';
+export { parseSyntax } from './parser';
+export { CppParserStrategy } from './ParserStrategy';
